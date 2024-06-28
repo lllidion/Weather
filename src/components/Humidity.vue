7fa166ffb8daa0652e78cbeab90b82e599318811
lllidion <lidka8939@gmail.com>
2024-06-28 13:32:52 +1000

    Приложение погода vue

---
+++ src/components/Humidity.vue
@@ -0,0 +1,31 @@
+<script setup>
+const props = defineProps({
+    humidity:{
+        types: Number,
+        required: true,
+    }
+})
+</script>
+
+
+<template>
+<section class="section-bottom">
+                <div class="block-bottom">
+                  <div class="block-bottom-inner">
+                    <div class="block-bottom-pic pic-humidity"></div>
+                    <div class="block-bottom-texts">
+                      <div class="block-bottom-text-block">
+                        <div class="block-bottom-text-block-title">
+                          Влажность: {{ humidity }} %
+                        </div>
+                        <div class="block-bottom-text-block-desc">
+                          Влажность – это концентрация водяного пара, присутствующего в воздухе. Водяной пар, газообразное состояние воды, обычно невидим для человеческого глаза.
+                          <!-- <br/><br /> -->
+                          Одно и то же количество водяного пара приводит к более высокой относительной влажности в прохладном воздухе, чем в теплом.
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </section> 
+</template>
---
+++ src/constants/index.js
@@ -0,0 +1,2 @@
+export const API_KEY = '1be350aba918a167ab21d080c29021ea'
+export const BASE_URL = 'https://api.openweathermap.org/data/2.5/weather'
